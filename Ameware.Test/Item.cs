e02f4658bfca4e23fce1851737a21a2e99d0e3eb
Dmitrii Fedosov <bossmen5@mail.ru>
2020-11-25 21:35:38 +0300

    Modified C#_record comment

--- Ameware.Test/Item.cs
+++ Ameware.Test/Item.cs
@@ -1,6 +1,10 @@
 ﻿namespace Ameware.Test
 {
-	public record Item//(int Id, string Text, int Parent)
+	/*
+	 * Records require at least init to be filled from JSON.
+	 * Also there could be something like 'public record Item(int Id, string Text, int Parent) {}'
+	 */
+	public record Item
 	{
 		public int Id { get; init; }
 		public string Text { get; init; }
